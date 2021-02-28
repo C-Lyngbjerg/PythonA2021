b1a34ab78eabee41ac99e7418f5b7e0de3bf44bd
C-Lyngbjerg <56966598+C-Lyngbjerg@users.noreply.github.com>
2021-02-28 13:29:42 +0100

    "Third attempt at python git commit script"

---
+++ GitCom.py
@@ -0,0 +1,9 @@
+import subprocess
+
+subprocess.run(['git', 'add', '-A'])
+print("Hello, please enter your commit message below:\n")
+comMsg = input()
+subprocess.run(['git', 'commit', '-m','"'+comMsg+'"'])
+subprocess.run (['git', 'push'])
+
+# https://github.com/C-Lyngbjerg/PythonA2021
